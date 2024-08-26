4d0183b8c9c02894b1b355c776ee53d1856bfaf5
Wellick0000 <kashyapshiven2002@gmail.com>
2024-08-26 13:44:22 +0530

    removed some line of code

--- server.js
+++ server.js
@@ -57,5 +57,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-console.log('Email User:', process.env.EMAIL_USER);
-console.log('Email Pass:', process.env.EMAIL_PASS);
