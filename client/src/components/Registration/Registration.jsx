b6ee9824e95abb25b69ab46516c4111edc0104b7
Wellick0000 <kashyapshiven2002@gmail.com>
2024-08-27 20:35:51 +0530

    updated table

--- client/src/components/Registration/Registration.jsx
+++ client/src/components/Registration/Registration.jsx
@@ -6,64 +6,101 @@ function Regestration() {
       {/* <p className="mb-5 text-center">
         Authors whose papers are accepted and are of high standards and having practical data/implementation will have the option to submit additional fees to get their paper published in UGC/Scopus indexed journal/book chapter apart from the Registration fees as mentioned in the category below:
       </p> */}
+      <div className="container mx-auto py-3 bg-cover bg-no-repeat w-full" style={{ backgroundImage: 'url(https://chandigarhengineeringcollege.com/public/course/assets/images/cec/club-bg.png)' }}>
+      <h1 className="text-3xl font-bold mb-5 text-center">REGISTRATION</h1>
       <div className="overflow-x-auto mb-5">
-      <table className="min-w-full bg-transparent border border-gray-200 shadow-lg rounded-lg">
-  <thead className="bg-gray-100 ">
-    <tr>
-      <th className="py-2 px-4 text-center" colSpan="1"></th>
-      <th className="py-2 px-4 text-center font-bold" colSpan="2">Early Bird Registration</th>
-      <th className="py-2 px-4 text-center font-bold" colSpan="3">Late Registration</th>
-    </tr>
-    <tr>
-      <th className="py-2 px-4 text-center font-semibold">Category of Authors/Attendees (IEEE Member)</th>
-      <th className="py-2 px-4 text-center font-semibold">IEEE Student Members</th>
-      <th className="py-2 px-4 text-center font-semibold">IEEE Members</th>
-      <th className="py-2 px-4 text-center font-semibold">IEEE Student Members</th>
-      <th className="py-2 px-4 text-center font-semibold">IEEE Members</th>
-    </tr>
-  </thead>
-  <tbody className="bg-white">
-    <tr className="hover:bg-gray-100">
-      <td className="py-2 px-4 text-center">Indian Participant</td>
-      <td className="py-2 px-4 text-center">₹5,000</td>
-      <td className="py-2 px-4 text-center">₹6,500</td>
-      <td className="py-2 px-4 text-center">₹6,000</td>
-      <td className="py-2 px-4 text-center">₹8,000</td>
-    </tr>
-    <tr className="hover:bg-gray-100">
-      <td className="py-2 px-4 text-center">Foreign Participant</td>
-      <td className="py-2 px-4 text-center">$150</td>
-      <td className="py-2 px-4 text-center">$250</td>
-      <td className="py-2 px-4 text-center">$250</td>
-      <td className="py-2 px-4 text-center">$300</td>
-    </tr>
-    <tr className="bg-gray-100">
-      <td className="py-2 px-4 text-center font-semibold">Category of Authors/Attendees (Non-IEEE Member)</td>
-      <td className="py-2 px-4 text-center font-semibold">Non-IEEE Student Members</td>
-      <td className="py-2 px-4 text-center font-semibold">Non-IEEE Members</td>
-      <td className="py-2 px-4 text-center font-semibold">Non-IEEE Student Members</td>
-      <td className="py-2 px-4 text-center font-semibold">Non-IEEE Members</td>
-    </tr>
-    <tr className="hover:bg-gray-100">
-      <td className="py-2 px-4 text-center">Indian Participant</td>
-      <td className="py-2 px-4 text-center">₹6,000</td>
-      <td className="py-2 px-4 text-center">₹8,500</td>
-      <td className="py-2 px-4 text-center">₹7,500</td>
-      <td className="py-2 px-4 text-center">₹10,000</td>
-    </tr>
-    <tr className="hover:bg-gray-100">
-      <td className="py-2 px-4 text-center">Foreign Participant</td>
-      <td className="py-2 px-4 text-center">$200</td>
-      <td className="py-2 px-4 text-center">$350</td>
-      <td className="py-2 px-4 text-center">$400</td>
-      <td className="py-2 px-4 text-center">$450</td>
-    </tr>
-  </tbody>
-</table>
+        <table className="min-w-full bg-transparent border border-gray-200 shadow-lg rounded-lg">
+          <thead>
+            <tr>
+              <th className="px-4 py-2 border-b">Delegates</th>
+              <th colSpan={2} className="px-4 py-2 border-b">Early Bird Registration</th>
+              <th colSpan={2} className="px-4 py-2 border-b">Late/Normal Registration</th>
+            </tr>
+            <tr>
+              <th className="px-4 py-2 border-b">Category of Author</th>
+              <th className="px-4 py-2 border-b">IEEE Member</th>
+              <th className="px-4 py-2 border-b">IEEE Non-Member</th>
+              <th className="px-4 py-2 border-b">IEEE Member</th>
+              <th className="px-4 py-2 border-b">IEEE Non-Member</th>
+              <th className="px-4 py-2 border-b">Attendee only</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="px-4 py-2 border-b font-bold text-center" colSpan="6" >Indian Participant (in INR)</td>
+            </tr>
+            <tr>
+              <td className="px-4 py-2 border-b">Student (UG/PG)</td>
+              <td className="px-4 py-2 border-b ">3000</td>
+              <td className="px-4 py-2 border-b">4000</td>
+              <td className="px-4 py-2 border-b">3500</td>
+              <td className="px-4 py-2 border-b">4500</td>
+              <td className="px-4 py-2 border-b">500</td>
+            </tr>
+            <tr>
+              <td className="px-4 py-2 border-b">Research Scholar (PhD)</td>
+              <td className="px-4 py-2 border-b">4500</td>
+              <td className="px-4 py-2 border-b">5500</td>
+              <td className="px-4 py-2 border-b">5500</td>
+              <td className="px-4 py-2 border-b">6500</td>
+              <td className="px-4 py-2 border-b">1000</td>
+            </tr>
+            <tr>
+              <td className="px-4 py-2 border-b">Academica/Faculty</td>
+              <td className="px-4 py-2 border-b">5500</td>
+              <td className="px-4 py-2 border-b">6500</td>
+              <td className="px-4 py-2 border-b">6500</td>
+              <td className="px-4 py-2 border-b">7500</td>
+              <td className="px-4 py-2 border-b">1000</td>
+            </tr>
+            <tr>
+              <td className="px-4 py-2 border-b">Delegates from Industry</td>
+              <td className="px-4 py-2 border-b">7000</td>
+              <td className="px-4 py-2 border-b">8000</td>
+              <td className="px-4 py-2 border-b">8000</td>
+              <td className="px-4 py-2 border-b">9000</td>
+              <td className="px-4 py-2 border-b">1500</td>
+            </tr>
+            <tr>
+              <td className="px-4 py-2 border-b font-bold text-center" colSpan="6" >Foreign Participant (in USD)</td>
+            </tr>
+            <tr>
+              <td className="px-4 py-2 border-b">Student (UG/PG)</td>
+              <td className="px-4 py-2 border-b">150</td>
+              <td className="px-4 py-2 border-b">200</td>
+              <td className="px-4 py-2 border-b">200</td>
+              <td className="px-4 py-2 border-b">250</td>
+              <td className="px-4 py-2 border-b">50</td>
+            </tr>
+            <tr>
+              <td className="px-4 py-2 border-b">Research Scholar (PhD)</td>
+              <td className="px-4 py-2 border-b">200</td>
+              <td className="px-4 py-2 border-b">250</td>
+              <td className="px-4 py-2 border-b">300</td>
+              <td className="px-4 py-2 border-b">350</td>
+              <td className="px-4 py-2 border-b">100</td>
+            </tr>
+            <tr>
+              <td className="px-4 py-2 border-b">Academica/Faculty</td>
+              <td className="px-4 py-2 border-b">300</td>
+              <td className="px-4 py-2 border-b">350</td>
+              <td className="px-4 py-2 border-b">400</td>
+              <td className="px-4 py-2 border-b">450</td>
+              <td className="px-4 py-2 border-b">100</td>
+            </tr>
+            <tr>
+              <td className="px-4 py-2 border-b">Delegates from Industry</td>
+              <td className="px-4 py-2 border-b">400</td>
+              <td className="px-4 py-2 border-b">450</td>
+              <td className="px-4 py-2 border-b">500</td>
+              <td className="px-4 py-2 border-b">550</td>
+              <td className="px-4 py-2 border-b">150</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-      <i className="flex text-center justify-center mb-5">
-        Registration fee as stated above can be paid through NEFT/Online transaction and refers to:
-      </i>
+      <i className=" text-sm mt-4 text-right">*Selected quality papers will be sent for publication in journals of Repute. Additional cost if any, for the publication has to be borne by the researcher.</i>
+    </div>
       <div className="bg-gray-100 p-5 rounded-lg shadow-lg">
         <p className="mb-2"><strong>Account Name:</strong> Chandigarh Engineering College</p>
         <p className="mb-2"><strong>Bank Name:</strong> State Bank of India</p>
